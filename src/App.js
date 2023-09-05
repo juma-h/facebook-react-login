140e0402c345f0acefe297b0efb6316916c55bb5
Juma-h <mitchfibi@gmail.com>
2023-09-05 16:44:14 +0300

    finally it works,problem was in the scope

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FacebookLogin from "react-facebook-login";
+
+import "./App.css";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+
+  // const responseFacebook = (response) => {
+
+  //   console.log("response from Facebook", response);
+  //   if (response.accessToken) {
+  //     setData(response);
+  //     setPicture(response.picture.data.url);
+  //     setAccessToken(response.accessToken);
+  //     setLogin(true);
+  //   } else {
+  //     // Handle the case where no access token is received
+  //     alert("No access token received");
+  //   }
+  // };
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    // Login failed
+    if (response.status === "unknown") {
+      alert("Login failed!");
+      setLogin(false);
+      return false;
+    }
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+      setAccessToken(response.accessToken);
+    } else {
+      setLogin(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div style={{ width: "600px" }}>
+        <div>
+          {!login && (
+            <FacebookLogin
+              appId="2227338407463775"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile"
+              callback={responseFacebook}
+              icon="fa-facebook"
+            />
+          )}
+          {login && <img alt="" src={picture} roundedCircle />}
+        </div>
+        {login && (
+          <div>
+            <h2>{data.name}</h2>
+            <p>{data.email}</p>
+            <p>Access Token: {accessToken}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
